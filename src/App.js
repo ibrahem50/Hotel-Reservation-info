379074d19b980c0fbcf41e274301174c5049e627
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-03-01 16:13:22 +0200

    added style to navbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import {BrowserRouter , Route , Switch} from 'react-router-dom'
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-       <Switch>
-         <Route exact path='/' component={Home} />
-         <Route exact path='/rooms/' component= {Rooms} />
-         <Route exact path='/rooms/:slug' component= {SingleRoom} />
-         <Route component={Error} />
-       </Switch>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/rooms/' component= {Rooms} />
+          <Route exact path='/rooms/:slug' component= {SingleRoom} />
+          <Route component={Error} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
