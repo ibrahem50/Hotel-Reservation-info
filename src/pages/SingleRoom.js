ec65b72f2797932e1d7c9c9a1df3b9f128264291
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-03-03 14:44:11 +0200

    added styledhero component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
+//import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 import { RoomContext } from "../context";
+import StyledHero from '../components/StyledHero'
 
 
 class SingleRoom extends Component {
@@ -41,13 +42,13 @@ class SingleRoom extends Component {
       const [main, ...defaultImages] = images;
       console.log(defaultImages);
     return (
-        <Hero hero='roomsHero'>
+        <StyledHero img={images[0]}>
             <Banner title={`${name} room`}>
                 <Link to='/rooms' className='btn-primary'>
                     back to rooms
                 </Link>
             </Banner>
-        </Hero>
+        </StyledHero>
     )
     }
 }
