c9fec39541281c7a31aaba75556829d45eb49fe9
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-03-03 18:16:39 +0200

    added filter by breakfast

--- src/context.js
+++ src/context.js
@@ -81,8 +81,22 @@ export class RoomProvider extends Component {
         price = parseInt(price);
         // filter by type
         if (type !== "all") {
-          tempRooms = tempRooms.filter(room => room.type === type);
-        }
+            tempRooms = tempRooms.filter(room => room.type === type);
+        };
+        //filter by capacity
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+        };
+        // filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price);
+        //filter by size
+        tempRooms = tempRooms.filter(
+           room => room.size >= minSize && room.size <= maxSize
+        );
+        //filter by breakfast
+        if (breakfast) {
+          tempRooms = tempRooms.filter(room => room.breakfast === true);
+        };
     };
     render() { 
         return ( 
