2e79c4e831baad3702a12c843f1fe8422ffa463d
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-03-03 16:40:59 +0200

    added RoomList Component

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,23 @@
-const RoomList = () => {
-    return ( 
-        <div>
-            Hello from room list
-        </div>
-     );
-}
- 
-export default RoomList;
+import Room from "./Room";
+
+const RoomsList = ({ rooms }) => {
+   if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  }
+  
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map(item => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default RoomsList;
